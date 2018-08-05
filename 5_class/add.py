8a3710910ef436fcbc6ecf45d92b4c54e3bdcb32
griezmanna8 <793650121@qq.com>
2018-08-05 20:12:07 +0800

    5_class

---
+++ 5_class/add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+print 'wogan'
+
+def Add(x, y):
+    return x + y
+
---
+++ 5_class/test.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# 人生苦短，我用 Python
+
+# header_only
+
+# import add
+# ret = add(1, 2)
+# print ret
+
+# import sys
+# print sys.path
+# sys.path.append('..')
+
+# from add import Add
+# print Add(1, 2)
+
+# import add
+# import add
+
+# import add
+# reload (add)
+
+# print globals()
+
+# class Test(object):
+#     'doc string'
+#     pass
+ 
+# t = Test()
+# print type(t)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
